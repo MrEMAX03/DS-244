3d0360cc92168fd9a67b192df80f9198cf75950a
Emanuele Bossi <emab03@gmail.com>
2025-02-04 14:39:49 -0700

    Update 2: added for loop

--- some_code.py
+++ some_code.py
@@ -1,3 +1,6 @@
 print("Hello!")
 print("This is the source code for DS 244-Assignment 03")
 print("Hmmm...I am trying to modify the source code for the 1st time!")
+for i in range(3):
+    if i==2:
+        print(f"This is commit nr.{i}")
